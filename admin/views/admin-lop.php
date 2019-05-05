aa1fa0443827fdac1b222ea954fb5908fd88ae88
lam.tran <lamtranduc2704@gmail.com>
2019-05-05 16:21:44 +0700

    update lop hoc

--- admin/views/admin-lop.php
+++ admin/views/admin-lop.php
@@ -123,7 +123,7 @@
                 <div class="card-header border-bottom">
                     <form action="admin-loaitin.php" method="get">
                         <h5 class="text-info">Danh sách lớp học</h5>
-                        <button type="button" name="them" data-toggle="modal" data-target="#myModal" class="btn btn-info">Thêm mới lớp học</button>
+                        <button id="btn-show-add" type="button" name="them" data-toggle="modal" data-target="#myModal" class="btn btn-info">Thêm mới lớp học</button>
                     </form>
 
                     <!-- Modal -->
@@ -137,6 +137,7 @@
                                     <button type="button" class="close" data-dismiss="modal">&times;</button>
                                 </div>
                                 <div class="modal-body">
+                                    <input id="id_chi_tiet_lop_hoc" type="hidden" value="">
                                     <div class="form-group">
                                         <label style="display:block">Tên lớp</label>
                                         <input name="ten_lop" onkeyup="check_ten_lop(this)" type="text" class="form-control">
@@ -172,7 +173,7 @@
                                     </div>
                                 </div>
                                 <div class="modal-footer">
-                                    <button id="btn-save" onclick="insert_lop_hoc()" type="button" class="btn btn-success"><i class="glyphicon glyphicon-floppy-saved"></i> Lưu lại</button>
+                                    <button id="btn-save" onclick="submit_lop_hoc()" type="button" class="btn btn-success"><i class="glyphicon glyphicon-floppy-saved"></i> Lưu lại</button>
                                     <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
                                 </div>
                             </div>
@@ -244,7 +245,9 @@
                                 <td><?php echo $item['sl_nhan_vien']?></td>
                                 <td><?php echo $item['sl_be']?></td>
                                 <td>
-                                    <a href="admin-loaitin-sua.php?id=<?php echo $item['id']; ?>"><i class="material-icons action-icon">edit</i></a>
+                                    <a onclick="show_form_edit(<?php echo $item['id']?>)" style="cursor: pointer" title="Cập nhật lớp học">
+                                        <i class="material-icons action-icon">edit</i>
+                                    </a>
                                     <a onclick="delete_lop_hoc(<?php echo $item['id']?>)" style="cursor: pointer" title="Xóa lớp học"><i class="material-icons action-icon">delete_outline</i></a>
                                 </td>
                             </tr>
@@ -260,14 +263,14 @@
                         //Nếu không phải là trang đầu thì hiện thị trang trước
                         if($current_page !=1)
                         {
-                            echo "<li class='page-item' class='float-left'><a class='page-link' href='admin-loaitin.php?s=".($start - $limit)."&p={$per_page}'>Trở về</a></li>";
+                            echo "<li class='page-item' class='float-left'><a class='page-link' href='admin-lop.php?s=".($start - $limit)."&p={$per_page}'>Trở về</a></li>";
                         }
                         //hiện thị những phần còn lại của trang
                         for ($i=1; $i <= $per_page ; $i++)
                         {
                             if($i != $current_page)
                             {
-                                echo "<li class='page-item'><a class='page-link' href='admin-loaitin.php?s=".($limit *($i - 1))."&p={$per_page}'>{$i}</a></li>";
+                                echo "<li class='page-item'><a class='page-link' href=admin-lop.php?s=".($limit *($i - 1))."&p={$per_page}'>{$i}</a></li>";
                             }
                             else
                             {
@@ -277,7 +280,7 @@
                         //Nếu không phải trang cuối thì hiện thị nút next
                         if($current_page != $per_page)
                         {
-                            echo "<li class='page-item' ><a class='page-link' href='admin-loaitin.php?s=".($start + $limit)."&p={$per_page}'>Tiếp</a></li>";
+                            echo "<li class='page-item' ><a class='page-link' href='admin-lop.php?s=".($start + $limit)."&p={$per_page}'>Tiếp</a></li>";
                         }
                     }
                     echo "</ul>";
@@ -290,7 +293,7 @@
     </div>
     <!-- End Default Light Table -->
 
-
+    <input type="hidden" id="flag_insert_update" value="1">
 
 </div>
 <!-- End page content-->
@@ -302,7 +305,19 @@
             text: 'Vui lòng chọn nhân viên'
         },
         maximumSelectionLength: 3
-    } )
+    } );
+
+    $(document).ready(function () {
+       $('#btn-show-add').click(function () {
+           $('#flag_insert_update').val(1); //bật cờ báo là đang ở form thêm mới lớp học
+           // gán gia trị text về null để thêm mới
+           $('#id_chi_tiet_lop_hoc').val("");
+           $('input[name="ten_lop"]').val("");
+           $('select[name="select_nien_khoa"]').val("");
+           $('select[name="select_lop_hoc"]').val("");
+           $('.select-nhannien').val("").trigger('change');
+       })
+    });
 
     function check_nhan_vien_da_lam_o_lop_khac() {
         $.get( "admin-xuly-lop.php?ten_lop=" + "aaa" + "&check_lop=1", function( data ) {
@@ -311,7 +326,6 @@
     
     function check_ten_lop(item) {
         $.get( "admin-xuly-lop.php?ten_lop=" + $(item).val() + "&check_lop=1", function( data ) {
-            console.log(data);
             if(data == "1"){
                 $('.erro-ten-lop').css('display','block');
                 $('#btn-save').attr('disabled','disabled');
@@ -351,7 +365,7 @@
             data = {
                 'delete': 1,
                 'id_chi_tiet_lop_hoc': id
-            }
+            };
             $.ajax({
                 type: "POST",
                 url: 'admin-xuly-lop.php',
@@ -362,6 +376,64 @@
                     location.reload();
                 }
             });
+        }
+    }
+
+    function show_form_edit(id) {
+        $('#flag_insert_update').val(2); //bật cờ báo là đang ở form cập nhật lớp học
+        // load thông tin của một lớp học
+        data = {
+            'load_info_item': 1,
+            'id_chi_tiet_lop_hoc': id
+        }
+        $.ajax({
+            type: "POST",
+            url: 'admin-xuly-lop.php',
+            data: data,
+            success : function (result){
+                item = JSON.parse(result);
+                $('#id_chi_tiet_lop_hoc').val(item.id);
+                $('input[name="ten_lop"]').val(item.mo_ta);
+                $('select[name="select_nien_khoa"]').val(item.nien_khoa_id);
+                $('select[name="select_lop_hoc"]').val(item.lop_hoc_id);
+                $('.select-nhannien').val(item.nv).trigger('change');
+            }
+        });
+        $('#myModal').modal().show();
+    }
+
+    function edit_lop_hoc(id) {
+        data = {
+            edit: 1,
+            ten_lop: $('input[name="ten_lop"]').val(),
+            id_nien_khoa: $('select[name="select_nien_khoa"]').val(),
+            id_lop: $('select[name="select_lop_hoc"]').val(),
+            id_chi_tiet_lop: $('#id_chi_tiet_lop_hoc').val(),
+            arr_nhan_vien: $('.select-nhannien').val()
+        };
+
+        $.ajax({
+            type: "POST",
+            url: 'admin-xuly-lop.php',
+            data: data,
+            success : function (result){
+                if (result == "1") {
+                    alert("Cập nhật thông tin lớp học thành công!");
+                }
+                else alert("Lỗi không cập nhật được lớp học");
+                location.reload();
+            }
+        });
+    }
+
+    function submit_lop_hoc() {
+        type = $('#flag_insert_update').val();
+        if(type == 1) {
+            insert_lop_hoc();
+        }
+        else{
+            id_chi_tiet_lop_hoc = $('#id_chi_tiet_lop').val();
+            edit_lop_hoc(id_chi_tiet_lop_hoc);
         }
     }
 </script>
--- admin/views/admin-xuly-lop.php
+++ admin/views/admin-xuly-lop.php
@@ -19,7 +19,7 @@ if (isset($_POST['add'])) {
     $query = "INSERT INTO lophoc_chitiet (lop_hoc_id, nien_khoa_id, mo_ta) VALUES ({$id_lop}, {$id_nien_khoa}, '{$ten_lop}')";
     $reslut = mysqli_query($dbc, $query);
     if ($reslut) {
-        if (is_array($arr_nhan_vien)) {
+        if (is_array($arr_nhan_vien) && count($arr_nhan_vien) > 0) {
             // lấy id lớp học mới được thêm vào
             $id_lop_hoc_moi_them = mysqli_fetch_row(mysqli_query($dbc, "SELECT id FROM lophoc_chitiet ORDER BY id DESC LIMIT 1"));
 
@@ -41,4 +41,51 @@ if(isset($_POST['delete']) && isset($_POST['id_chi_tiet_lop_hoc'])) {
     $query = "DELETE FROM lophoc_chitiet WHERE id = {$id}";
     if(mysqli_query($dbc, $query))  echo 1;
     else echo -1;
+}
+
+// Load chi tiết một lớp học
+if (isset($_POST['load_info_item']) && isset($_POST['id_chi_tiet_lop_hoc'])) {
+    $id = (int)$_POST['id_chi_tiet_lop_hoc'];
+    //Load
+    $data = mysqli_fetch_assoc(mysqli_query($dbc, "SELECT * FROM lophoc_chitiet WHERE id = {$id}"));
+
+    // Load thông tin nhân viên của lớp học
+    $nhan_vien = (mysqli_query($dbc, "SELECT nhan_vien_id FROM lophoc_nhanvien WHERE lop_hoc_chi_tiet_id = {$id}"));
+    $ar_nv = [];
+    foreach ($nhan_vien as $item) {
+        $ar_nv[] = $item['nhan_vien_id'];
+    }
+    $data['nv'] = $ar_nv;
+
+    echo json_encode($data);
+}
+
+// Cập nhật lớp học
+if (isset($_POST['edit'])) {
+    $ten_lop = $_POST['ten_lop'];
+    $id_lop = $_POST['id_lop'];
+    $id_chi_tiet_lop = $_POST['id_chi_tiet_lop'];
+    $id_nien_khoa = $_POST['id_nien_khoa'];
+    $arr_nhan_vien = isset($_POST['arr_nhan_vien']) ? (array)$_POST['arr_nhan_vien'] : [];
+
+    $query = "UPDATE lophoc_chitiet
+              SET lop_hoc_id = {$id_lop}, nien_khoa_id = {$id_nien_khoa}, mo_ta = '{$ten_lop}'
+              WHERE id = {$id_chi_tiet_lop} 
+              ";
+    $reslut = mysqli_query($dbc, $query);
+    if ($reslut) {
+        if (count($arr_nhan_vien) > 0) {
+            // Xóa những nhân viên cũ trong lớp
+            mysqli_query($dbc, "DELETE FROM lophoc_nhanvien WHERE lop_hoc_chi_tiet_id = {$id_chi_tiet_lop}");
+            $query_xoa_nhan_vien = mysqli_affected_rows($dbc);
+
+            // thêm nhân viên vào lớp học
+            for ($i = 0; $i < count($arr_nhan_vien); $i++) {
+                $query_insert = "INSERT INTO lophoc_nhanvien (nhan_vien_id, lop_hoc_chi_tiet_id) VALUES ({$arr_nhan_vien[$i]}, {$id_chi_tiet_lop})";
+
+                mysqli_query($dbc, $query_insert);
+            }
+        }
+        echo 1;
+    } else echo -1;
 }
